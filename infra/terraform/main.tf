f577ad8e1d51b1b002f9c3cb4f16cae29acba550
Illia Savchenko <ilas70587@gmail.com>
2025-10-09 19:12:54 +0200

    Deploy helm and karpenter modules

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -14,7 +14,7 @@ locals {
   azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
   tags = {
-    Project      = local.name
+    Project   = local.name
     DevOps    = "KastonL"
     ManagedBy = "Terraform"
     GithubOrg = "terraform-aws-modules"
@@ -23,7 +23,7 @@ locals {
 
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
-  version = "~> 6.0"
+  version = "6.4.0"
 
   name = local.name
   cidr = local.vpc_cidr
@@ -42,8 +42,95 @@ module "vpc" {
 
   private_subnet_tags = {
     "kubernetes.io/role/internal-elb" = 1
+    "karpenter.sh/discovery"          = local.name
   }
 
   tags = local.tags
 }
 
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "21.3.2"
+
+  name               = local.name
+  kubernetes_version = local.kubernetes_version
+
+  enable_cluster_creator_admin_permissions = true
+  endpoint_public_access                   = true
+
+  addons = {
+    coredns = {}
+    eks-pod-identity-agent = {
+      before_compute = true
+    }
+    kube-proxy = {}
+    vpc-cni = {
+      before_compute = true
+    }
+  }
+
+  vpc_id                   = module.vpc.vpc_id
+  subnet_ids               = module.vpc.private_subnets
+  control_plane_subnet_ids = module.vpc.intra_subnets
+
+  eks_managed_node_groups = {
+    karpenter = {
+      ami_type       = "BOTTLEROCKET_x86_64"
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 1
+      desired_size = 1
+
+      labels = {
+        "karpenter.sh/controller" = "true"
+      }
+    }
+  }
+  node_security_group_tags = merge(local.tags, {
+    "karpenter.sh/discovery" = local.name
+  })
+
+  tags = local.tags
+}
+
+module "karpenter" {
+  source = "terraform-aws-modules/eks/aws//modules/karpenter"
+  version = "21.3.2"
+
+  cluster_name = module.eks.cluster_name
+
+  node_iam_role_use_name_prefix   = false
+  node_iam_role_name              = local.name
+  create_pod_identity_association = true
+
+  node_iam_role_additional_policies = {
+    AmazonSSMManagedInstanceCore = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+  }
+
+  tags = local.tags
+}
+
+resource "helm_release" "karpenter" {
+  namespace           = "kube-system"
+  name                = "karpenter"
+  repository          = "https://charts.karpenter.sh"
+  chart               = "karpenter"
+  version             = "1.6.0"
+  wait                = false
+
+  values = [
+    <<-EOT
+    nodeSelector:
+      karpenter.sh/controller: 'true'
+    dnsPolicy: Default
+    settings:
+      clusterName: ${module.eks.cluster_name}
+      clusterEndpoint: ${module.eks.cluster_endpoint}
+      interruptionQueue: ${module.karpenter.queue_name}
+    webhook:
+      enabled: false
+    EOT
+  ]
+}
