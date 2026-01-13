eb02ee02ef6a8a744aa307f1128754a1c424d4cd
Illia Savchenko <ilas70587@gmail.com>
2026-01-13 15:09:43 +0100

    Fast fix №3

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -15,7 +15,7 @@ locals {
   region             = "eu-central-1"
 
   vpc_cidr = "10.0.0.0/16"
-  azs      = slice(data.aws_availability_zones.available.names, 0, 2)
+  # azs      = slice(data.aws_availability_zones.available.names, 0, 2)
 
   tags = {
     Project   = local.name
